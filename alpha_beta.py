cbd3bc1cefad50fd9129baf6bfa0cdf9dee8d38a
timothy-clay <tjc0426@gmail.com>
2025-10-31 02:30:29 -0400

    working minimax!

--- alpha_beta.py
+++ alpha_beta.py
@@ -9,7 +9,58 @@ def heuristic(players, grid):
     player1_path = player1._getShortestPath(grid)
     player2_path = player2._getShortestPath(grid)
 
-    return player2_path - player1_path # player 1 wants to maximize, player 2 wants to minimize
+    player1_win = 10 * int(player1.row == player1.target_row)
+    player2_win = 10 * int(player2.row == player2.target_row)
+
+    return player1_win + player2_path - player1_path - player2_win # player 1 wants to maximize, player 2 wants to minimize
+
+
+
+def minimax(game, winner, grid, players, depth, alpha, beta):
+
+    if depth==0 or winner is not None:
+        return heuristic(players, grid), None
+    
+    best_move = None
+    options = players['active_player'].getValidTurns(grid, players['inactive_player'])
+
+    if players['active_player'] == players['player1']:
+
+        max_heuristic = float('-inf')
+        for move in options:
+            new_game = game.duplicate()
+            new_winner, new_grid, new_players = new_game.execute(move)
+            eval, _ = minimax(new_game, new_winner, new_grid, new_players, depth - 1, alpha, beta)
+
+            if eval > max_heuristic:
+                max_heuristic = eval
+                best_move = move
+
+            alpha = max(alpha, eval)
+
+            if beta <= alpha:
+                break
+
+        return max_heuristic, best_move
+
+    else:
+
+        min_heuristic = float('inf')
+        for move in options:
+            new_game = game.duplicate()
+            new_winner, new_grid, new_players = new_game.execute(move)
+            eval, _ = minimax(new_game, new_winner, new_grid, new_players, depth - 1, alpha, beta)
+
+            if eval < min_heuristic:
+                min_heuristic = eval
+                best_move = move
+
+            beta = min(beta, eval)
+
+            if beta <= alpha:
+                break
+
+        return min_heuristic, best_move
 
 game = Quoridor(GUI=True, print_instructions=False, sleep=1, gs=9)
 
@@ -17,19 +68,10 @@ winner, grid, players = game.execute('e')
 
 winner = None
 while winner is None:
-    options = players['active_player'].getValidTurns(grid, players['inactive_player'])
 
-    max_heuristic = -100
-    for move in options:
-        new_game = game.duplicate()
-        test_winner, test_grid, test_players = new_game.execute(move)
+    score, best_move = minimax(game, winner, grid, players, depth=2, alpha=float('-inf'), beta=float('inf'))
 
-        test_heuristic = heuristic(test_players, test_grid)
-
-        if test_heuristic > max_heuristic:
-            best_move = move
-            max_heuristic = test_heuristic
-
+    print(score, best_move)
     winner, grid, players = game.execute(best_move)
 
 
